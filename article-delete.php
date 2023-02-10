f0ab6ba60186424817c747bf33be74546d6f4b85
Skypeace8585 <i.takahashi.sys22@morijyobi.ac.jp>
2023-02-10 11:32:30 +0900

    css update

---
+++ article-delete.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>閲覧画面</h1>
+    </header>
+    <footer>
+        
+    </footer>
+</body>
+</html>
