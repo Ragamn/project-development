9110cb319ec55b2a2408fdd55c21acff60fbd2fa
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2023-02-15 12:35:31 +0900

    ユーザ作成確認画面の作成

--- create-user-confirm.php
+++ create-user-confirm.php
@@ -1,5 +1,8 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
@@ -9,6 +12,10 @@
     <header>
         <h1>ユーザー作成確認画面</h1>
     </header>
+    ユーザー名:<?php echo $_SESSION['username'];?>
+    年齢:<?php echo $_SESSION['age'];?>
+    性別:<?php echo $_SESSION['gender'];?>
+    パスワード: *******
     <footer>
         
     </footer>
--- db_connect.php
+++ db_connect.php
@@ -21,7 +21,6 @@
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
         //例外がスローされるようにする
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        echo "データベースに接続しました";
     }catch (Exception $e){
         echo "DB接続エラー";
         echo $e->getMessage();
